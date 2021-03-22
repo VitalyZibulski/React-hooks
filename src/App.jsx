0e3b7f91832de1e7692682a3b5ea0608c39e27a3
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2021-03-22 23:14:55 +0300

    add hook useRequest

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
-import { useState } from "react";
-import useDebounce from "./hooks/useDebounce";
+import React from "react";
+import axios from 'axios';
+import useRequest from "./hooks/useRequest";
 
 function App() {
-  const [value, setValue] = useState('');
-  const debouncedSearch = useDebounce(search, 500);
-
-  function search(query) {
-    fetch(`https://jsonplaceholder.typicode.com/todos?query=` + query)
-      .then(response => response.json())
-      .then(json => console.log(json));
+  const [todos, loading, error] = useRequest(fetchTodos);
+  function fetchTodos() {
+    return axios.get(`https://jsonplaceholder.typicode.com/todos`);
   }
 
-  const onChange = e => {
-    setValue(e.target.value);
-    debouncedSearch(e.target.value);
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+
+  if (error) {
+    return <h1>Error</h1>
   }
 
   return (
     <div>
-      <input type="text" value={value} onChange={onChange}/>
+      {todos && todos.map(todo =>
+        <div key={todo.id} style={{padding: 30, border: '2px solid black'}}>
+          {todo.id}. {todo.title}
+        </div>
+      )}
     </div>
   );
 }
